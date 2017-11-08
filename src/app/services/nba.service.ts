9b78303298e9f8235db7962e8db7b3685c5bcdbe
Mi6u3l <miguelbragagomes@gmail.com>
2017-11-08 22:13:37 +0000

    create service component

---
+++ src/app/services/nba.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers, RequestOptions } from '@angular/http';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
+import { Observable } from 'rxjs/Rx';
+
+@Injectable()
+export class NbaService {
+  BASE_URL: string = 'https://stats.nba.com/stats';
+
+  constructor(
+    private http: Http
+  ) { }
+
+  handleError(e) {
+    return Observable.throw(e.json().message);
+  }
+
+  getList() {
+    return this.http.get(`${this.BASE_URL}/leaguedashplayerbiostats`)
+      .map((res) => res.json())
+      .catch(this.handleError);
+  }
+}
